ed94e8455c71cb64e8fb670410bc8ff90ac8a688
Andrew Allen <achew22@achew22.com>
2013-05-24 11:25:18 -0500

    Add google analytics

--- themes/andrewallen/static/js/main.js
+++ themes/andrewallen/static/js/main.js
@@ -1,7 +1,7 @@
 aza = function(){};
 aza.main = function() {
     return 0; // Exit code
-}
+};
 
 /**
  * @copyright Sky Sanders
@@ -18,7 +18,7 @@ aza.timeSince = function(since) {
 
     interval = Math.floor(seconds / 86400);
     if (interval > 3) {
-        var months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+        var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
         var dow = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
         var ord = "th";
@@ -47,4 +47,24 @@ aza.timeSince = function(since) {
         return interval + " minutes ago";
     }
     return Math.floor(seconds) + " seconds ago";
-}
+};
+
+
+aza.lab_experiment = function(url, div, init) {
+    console.log("Adding lab work from " + url);
+
+    var timeout = setTimeout(function() {
+        $(div).text("An error has occurred. Please try reloading the page");
+    }, 10000);
+
+    // It would be nice to do this but errors get eaten in cross domain
+    // So we are going to use a timeout of 5 sec
+////    }, 5000 /* ms */)
+
+    $.getScript(url)
+    .done(function(script, textStatus) {
+        window.clearTimeout(timeout);
+        console.log( textStatus );
+        init();
+    });
+};
